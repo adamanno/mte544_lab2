a2c67390073488736255f3b1383e21664704f589
notSoMechanical <96740031+notSoMechanical@users.noreply.github.com>
2022-11-16 11:28:07 -0500

    update to matlab file

--- lab2_readbag.m
+++ lab2_readbag.m
@@ -4,7 +4,14 @@ clc
 bag1 = ros2bagreader('point1/point1_0.db3');
 bag2 = ros2bagreader('point2/point2_0.db3');
 
-odom1 = select(bag1,"Topic","/odom");
-odom2 = select(bag2,"Topic","/odom");
+% odom1 = select(bag1,"Topic","/odom");
+% odom2 = select(bag2,"Topic","/odom");
 
-odom1_msgs = readMessages(odom1);
+% scan1 = select(bag1,"Topic","/scan");
+% scan2 = select(bag2,"Topic","/scan");
+
+odom1_msgs = readMessages(select(bag1,"Topic","/odom"));
+scan1_msgs = readMessages(select(bag1,"Topic","/scan"));
+
+tf_msgs = readMessages(select(bag1,"Topic","/tf"));
+tf_static_msgs = readMessages(select(bag1,"Topic","/tf_static"));
