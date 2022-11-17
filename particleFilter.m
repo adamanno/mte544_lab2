d036e90e3a6e0e39d0250f264225202667e17dde
notSoMechanical <96740031+notSoMechanical@users.noreply.github.com>
2022-11-17 14:04:28 -0500

    added new files

--- particleFilter.m
+++ particleFilter.m
@@ -2,30 +2,25 @@ clear
 clc
 close all
 
-load('topicData_point1.mat')
-load('topicData_point2.mat')
-
-odom1_msgs = cell2mat(odom1_msgs);
-odom2_msgs = cell2mat(odom2_msgs);
-
-scan1_msgs = cell2mat(scan1_msgs);
-scan2_msgs = cell2mat(scan2_msgs);
-
-tf1_msgs = cell2mat(tf1_msgs);
-tf2_msgs = cell2mat(tf2_msgs);
-
-tf_static1_msgs = cell2mat(tf_static1_msgs);
-tf_static2_msgs = cell2mat(tf_static2_msgs);
+load('lab2_data.mat')
 
 map = imread('map_mazes/map_maze_2.pgm');
-% imshow(map)
-f = createFreespace(map, 'uint8');
 figure(1)
+imshow(map)
+f = createFreespace(map, 'uint8');
+figure(2)
 imshow(f)
+imwrite(f, 'freespace.bmp','bmp')
 
 
+m=1000;
+p = createParticles(m,f,1);
 
-
+figure(3)
+hold on
+for n = 1:m
+    plot(p(n).y, p(n).x, '.r')
+end
 
 
 
