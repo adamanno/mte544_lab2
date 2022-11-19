d967d6a3a4642a96e0470580680fbfcf8fcf4c57
Dhruv <dhruvsandeepjain19@gmail.com>
2022-11-19 15:18:25 -0500

    modified create particles to certain bounds

--- particleFilter.m
+++ particleFilter.m
@@ -14,15 +14,15 @@ map_matlab.OccupiedThreshold = 0.65;
 map_matlab.FreeThreshold = 0.25;
 map_matlab.GridOriginInLocal = [-0.843 -3.94 ];
 
-map_matlab.XWorldLimits
-map_matlab.YWorldLimits
+x_range = map_matlab.XWorldLimits;
+y_range = map_matlab.YWorldLimits;
 
 m=1000;
 
 % modify createParticles to generate particles on the 
 % x = [2, xmax], y =[-y, ymin] intervals
 % theta = [0, 2*pi]
-p = createParticles(m,f,1);
+p = createParticles(m,x_range,y_range);
 
 
 figure(1)
@@ -31,7 +31,7 @@ map_matlab.show()
 hold off
 hold on
 for n = 1:m
-    plot(p(n).y, p(n).x, '.r')
+    plot(p(n).x, p(n).y, '.r')
 end
 hold off
 
